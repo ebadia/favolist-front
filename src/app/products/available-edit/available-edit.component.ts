b3d4b5565a37ff044e7e0b906341a649d6c843c7
ebadia <enric.badia@gmail.com>
2018-05-20 23:48:52 +0200

    fix: available edit stock quantities

--- src/app/products/available-edit/available-edit.component.ts
+++ src/app/products/available-edit/available-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms'
 import { AvailablesEditSubject } from '@services/subjects/availables-edit.subject'
 import { ActivatedRoute } from '@angular/router'
@@ -23,19 +23,17 @@ export class AvailableEditComponent implements OnInit {
     private _fb: FormBuilder,
     private _route: ActivatedRoute,
     private _service: AvailablesService,
-    private dialog: MatDialog,
+    private dialog: MatDialog
   ) {
-    this._availableEditSubject.availableEditAnnounced$.subscribe(
-      res => {
-        this.editing = res
-        this.product = JSON.parse( localStorage.getItem('edit-available') )
-        this.initForm()
-      }
-    )
+    this._availableEditSubject.availableEditAnnounced$.subscribe(res => {
+      this.editing = res
+      this.product = JSON.parse(localStorage.getItem('edit-available'))
+      this.initForm()
+    })
   }
 
   ngOnInit() {
-    this.product = JSON.parse( localStorage.getItem('edit-available') )
+    this.product = JSON.parse(localStorage.getItem('edit-available'))
     this.initForm()
   }
 
@@ -44,13 +42,13 @@ export class AvailableEditComponent implements OnInit {
     const data = this.product
     this.form = this._fb.group({
       name: [data ? data.name : '', Validators.required],
-      price: [ data ?
-        ( data.availablePrice ? data.availablePrice : data.price )
-        : 0],
+      price: [
+        data ? (data.availablePrice ? data.availablePrice : data.price) : 0
+      ],
       availableStock: [data ? data.availableStock : 0],
       availableStockOut: [data ? data.availableStockOut : 0],
       shopId: [data ? data.shopId : 0],
-      id: [data ? data.id : 0],
+      id: [data ? data.id : 0]
     })
     // console.log('FORM DATA >>> ', this.form)
   }
@@ -79,23 +77,20 @@ export class AvailableEditComponent implements OnInit {
 
   save() {
     // save available product
-    console.log('AVAILABLE SAVE', this.form.value )
+    console.log('AVAILABLE SAVE', this.form.value)
     const obj = {
-      stock: this.form.value.stock,
-      stockOut: this.form.value.stockOut,
-      price: this.form.value.price,
+      stock: this.form.value.availableStock,
+      stockOut: this.form.value.availableStockOut,
+      price: this.form.value.price
     }
-    this._service.update( this.product.availableId, obj).subscribe(
-      res => {
-        this._availableSubject.announceAvailable(true)
-        this.cancel()
-      }
-    )
+    this._service.update(this.product.availableId, obj).subscribe(res => {
+      this._availableSubject.announceAvailable(true)
+      this.cancel()
+    })
   }
 
   cancel() {
     localStorage.setItem('edit-available', null)
     this._availableEditSubject.announceEditAvailable(false)
-
   }
 }
